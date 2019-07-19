73a1bb844dabebae18b1cc48bfca156d46a84de6
kin <kangjie.cui@elementwin.com>
2019-07-19 18:54:01 +0800

    init

--- src/index.js
+++ src/index.js
@@ -6,31 +6,45 @@ function replaceHtml(str) {
   return str.replace(/<html\s.*?>/g, "<html>").replace(/&nbsp;/g, "");
 }
 
-request("https://www.23us.us/html/40/40021/")
+request("http://www.uctxt.com/book/1/1269/")
   .then(data => {
     let html = replaceHtml(data);
 
-    let doc = new dom({
+    let Dom = new dom({
       errorHandler: {
         warning: function(w) {
           console.log(1);
         },
-        error: () => {
+        error: error => {
           console.log(2);
         },
         fatalError: () => {
           console.log(3);
         }
       }
-    }).parseFromString(html, "text/xml");
+    });
 
-    let titles = xpath.select("//div[@class='listmain']//dd/a/[@href, text()]", doc);
+    let doc = Dom.parseFromString(html, "text/xml");
 
-    // titles = titles.map(item => {
-    //   return item.value;
-    // });
+    let titles = xpath.select(
+      `//dl[contains(@class, "chapter-list")]//dd/a`,
+      doc
+    );
 
-    console.log(titles);
+    let links = [];
+    for (let item of titles) {
+      let str = Dom.parseFromString(item.toString(), "text/xml");
+
+      let $href = xpath.select("string(//@href)", str);
+      let $text = xpath.select("string(//text())", str);
+
+      links.push({
+        href: $href,
+        text: $text
+      });
+    }
+
+    console.log(links);
   })
   .catch(err => {
     console.log(err);
